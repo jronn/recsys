790c50562d2ddf3ad5d056ca5c0bb1f73e20808b
jronn <josefronn@gmail.com>
2015-02-01 21:09:51 +0100

    added redirect to login (further config required for full use)

---
+++ src/main/java/se/kth/ict/iv1201/recsys/view/RegisterBean.java
@@ -0,0 +1,68 @@
+package se.kth.ict.iv1201.recsys.view;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import se.kth.ict.iv1201.recsys.controller.HelloEJB;
+
+
+@Named("registerBean")
+@SessionScoped
+public class RegisterBean implements Serializable {
+    
+    @EJB
+    HelloEJB helloEjb;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String username;
+    private String password;
+
+    public void register() {
+        //TODO
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+}
