4f294bc2bc18a5c50dafadda4bc35c5b673abf60
jronn <josefronn@gmail.com>
2015-02-23 19:09:37 +0100

    updated login

--- src/main/java/se/kth/ict/iv1201/recsys/view/Auth.java
+++ src/main/java/se/kth/ict/iv1201/recsys/view/Auth.java
@@ -2,23 +2,92 @@ package se.kth.ict.iv1201.recsys.view;
 
 import java.io.IOException;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 /**
- * Backing bean for authentication
+ * Backing bean for authentication when logging in / out
  * 
  * @author jronn
  */
 @Named("auth")
 @RequestScoped
 public class Auth implements Serializable {
+   
+    private String username;
+    private String password;
+    private String originalURL;
     
-   public void logout() throws IOException {
-        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-        ec.redirect(ec.getRequestContextPath() + "/faces/register.xhtml");
-   }   
+    
+    @PostConstruct
+    public void init() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        originalURL = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_REQUEST_URI);
+
+        if (originalURL == null) {
+            originalURL = externalContext.getRequestContextPath() + "/home.xhtml";
+        } else {
+            String originalQuery = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_QUERY_STRING);
+
+            if (originalQuery != null) {
+                originalURL += "?" + originalQuery;
+            }
+        }
+    }
+ 
+    /**
+     * Logs the user in and redirects to previous searched page
+     * @throws IOException 
+     */
+    public void login() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+
+        try {
+            request.login(username, password);
+            // user = userService.find(username, password);
+            //externalContext.getSessionMap().put("user", user);
+            externalContext.redirect(originalURL);
+        } catch (ServletException e) {
+            // Handle unknown username/password in request.login().
+            context.addMessage(null, new FacesMessage("Unknown login"));
+        }
+    }
+    
+    /**
+     * Logs the user out by invalidating the session and redirects to login page
+     * @throws IOException 
+     */
+    public void logout() throws IOException {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        externalContext.invalidateSession();
+        externalContext.redirect(externalContext.getRequestContextPath() + "/faces/login.xhtml");
+    }   
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
 }
